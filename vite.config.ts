e2e6998f19a2500165f911035e959789eec1f105
stray-code <abc1007020@gmail.com>
2024-04-14 20:20:22 +0900

    crxjs追加と設定

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { crx, defineManifest } from "@crxjs/vite-plugin";
+
+const manifest = defineManifest({
+  manifest_version: 3,
+  name: "ウィンドウサイズ変更",
+  description: "ワンクリックでウィンドウサイズ（画面サイズ）を変更できます。",
+  version: "1.0.0",
+  icons: {
+    16: "img/icon16.png",
+    48: "img/icon48.png",
+    128: "img/icon128.png",
+  },
+});
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), crx({ manifest })],
+  server: {
+    port: 5173,
+    strictPort: true,
+    hmr: {
+      port: 5173,
+    },
+  },
 })
