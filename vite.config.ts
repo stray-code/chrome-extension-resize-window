1fd7a1f5927d803f97b1d41ce3d3d77833ab5889
stray-code <abc1007020@gmail.com>
2024-04-24 20:35:34 +0900

    バージョン更新

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ const manifest = defineManifest({
   manifest_version: 3,
   name: "ウィンドウサイズ変更",
   description: "ワンクリックでウィンドウサイズ（画面サイズ）を変更できます。",
-  version: "1.0.0",
+  version: "1.0.1",
   icons: {
     16: "img/icon16.png",
     48: "img/icon48.png",
