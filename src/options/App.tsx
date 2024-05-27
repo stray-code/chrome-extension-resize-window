1b16a30830f33472c77a3e8a59d0f4dbc9b8a836
stray-code <abc1007020@gmail.com>
2024-05-27 22:28:05 +0900

    Stackで余白を作る

--- src/options/App.tsx
+++ src/options/App.tsx
@@ -58,81 +58,86 @@ function App() {
 
   return (
     <Container py="xl" size="xs">
-      <Title order={3}>ウィンドウサイズ設定</Title>
-      <Paper mt="xl" p="md" shadow="md" withBorder>
-        <form
-          onSubmit={form.onSubmit(async (values) => {
-            const newWindowSizeList = [...windowSizeList, values];
+      <Stack gap="xl">
+        <Title order={3}>ウィンドウサイズ設定</Title>
+        <Paper p="md" shadow="md" withBorder>
+          <form
+            onSubmit={form.onSubmit(async (values) => {
+              const newWindowSizeList = [...windowSizeList, values];
 
-            setWindowSizeList(newWindowSizeList);
+              setWindowSizeList(newWindowSizeList);
 
-            await setLocalStorage("windowSizeList", newWindowSizeList);
+              await setLocalStorage("windowSizeList", newWindowSizeList);
 
-            form.reset();
-          })}
-        >
-          <Stack>
-            <Text size="sm" c="gray">
-              モニタを超えるサイズは動作しないためご注意ください。
-            </Text>
-            <SimpleGrid cols={2}>
-              <TextInput
-                {...form.getInputProps("width")}
-                label="横幅"
-                placeholder="1200"
-              />
-              <TextInput
-                {...form.getInputProps("height")}
-                label="縦幅"
-                placeholder="800"
-              />
-            </SimpleGrid>
-            <Box>
-              <Button type="submit">登録</Button>
-            </Box>
-          </Stack>
-        </form>
-      </Paper>
-      <Table mt="xl">
-        <Table.Thead>
-          <Table.Tr>
-            <Table.Th>横幅</Table.Th>
-            <Table.Th>縦幅</Table.Th>
-            <Table.Th w={0} />
-          </Table.Tr>
-        </Table.Thead>
-        <Table.Tbody>
-          {windowSizeList.map((windowSize, index) => (
-            <Table.Tr key={index}>
-              <Table.Td>{windowSize.width}</Table.Td>
-              <Table.Td>{windowSize.height}</Table.Td>
-              <Table.Td>
-                <Button
-                  variant="subtle"
-                  color="red"
-                  onClick={async () => {
-                    const result = confirm("削除してもよろしいですか？");
-
-                    if (!result) {
-                      return;
-                    }
-
-                    const newWindowSizeList = windowSizeList.filter(
-                      (_, i) => i !== index,
-                    );
-
-                    setWindowSizeList(newWindowSizeList);
-
-                    await setLocalStorage("windowSizeList", newWindowSizeList);
-                  }}
-                >
-                  削除
-                </Button>
-              </Table.Td>
+              form.reset();
+            })}
+          >
+            <Stack>
+              <Text size="sm" c="gray">
+                モニタを超えるサイズは動作しないためご注意ください。
+              </Text>
+              <SimpleGrid cols={2}>
+                <TextInput
+                  {...form.getInputProps("width")}
+                  label="横幅"
+                  placeholder="1200"
+                />
+                <TextInput
+                  {...form.getInputProps("height")}
+                  label="縦幅"
+                  placeholder="800"
+                />
+              </SimpleGrid>
+              <Box>
+                <Button type="submit">登録</Button>
+              </Box>
+            </Stack>
+          </form>
+        </Paper>
+        <Table>
+          <Table.Thead>
+            <Table.Tr>
+              <Table.Th>横幅</Table.Th>
+              <Table.Th>縦幅</Table.Th>
+              <Table.Th w={0} />
             </Table.Tr>
-          ))}
-        </Table.Tbody>
-      </Table>
+          </Table.Thead>
+          <Table.Tbody>
+            {windowSizeList.map((windowSize, index) => (
+              <Table.Tr key={index}>
+                <Table.Td>{windowSize.width}</Table.Td>
+                <Table.Td>{windowSize.height}</Table.Td>
+                <Table.Td>
+                  <Button
+                    variant="subtle"
+                    color="red"
+                    onClick={async () => {
+                      const result = confirm("削除してもよろしいですか？");
+
+                      if (!result) {
+                        return;
+                      }
+
+                      const newWindowSizeList = windowSizeList.filter(
+                        (_, i) => i !== index,
+                      );
+
+                      setWindowSizeList(newWindowSizeList);
+
+                      await setLocalStorage(
+                        "windowSizeList",
+                        newWindowSizeList,
+                      );
+                    }}
+                  >
+                    削除
+                  </Button>
+                </Table.Td>
+              </Table.Tr>
+            ))}
+          </Table.Tbody>
+        </Table>
+      </Stack>
     </Container>
   );
 }
