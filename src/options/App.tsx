f53fb83f5dc9789d8f5e3e57d526847d579a2278
stray-code <abc1007020@gmail.com>
2025-06-01 18:25:41 +0900

    Revert "アンケートリンクを削除"
    This reverts commit 0f7f2f88c73f080d17753f865cb8f827f42988e1.

--- src/options/App.tsx
+++ src/options/App.tsx
@@ -1,6 +1,8 @@
 import {
+  Anchor,
   Button,
   Container,
+  Group,
   Paper,
   SimpleGrid,
   Stack,
@@ -89,6 +91,16 @@ function App() {
                   placeholder="800"
                 />
               </SimpleGrid>
+              <Group justify="space-between">
+                <Button type="submit">登録</Button>
+                <Anchor
+                  href="https://docs.google.com/forms/d/e/1FAIpQLSdj4ZEJvxDWwriXX7Bp52xMNYUQ4w2rGY7gWx8V395JelUL0A/viewform?usp=header"
+                  target="_blank"
+                  size="sm"
+                >
+                  アンケート
+                </Anchor>
+              </Group>
             </Stack>
           </form>
         </Paper>
